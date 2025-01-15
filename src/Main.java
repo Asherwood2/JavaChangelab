721ab6bc4cc0ebe5c714382e8f2a717897511b8e
Austin Sherwood <asherwood349@cub.uca.edu>
2025-01-15 10:46:48 -0600

    init commit

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+record Denomination(String name, Double amt, String form, String  img){}
+
+        }
+    }
---
+++ src/Purse.java
@@ -0,0 +1,9 @@
+public class Purse {
+    int cash;
+    public Purse(int cash){
+        cash = 0;
+    }
+    public void add(int cash){
+
+    }
+}
