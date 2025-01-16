e91e411526fd14309459a4eb28c25564ea0c730e
Austin Sherwood <asherwood349@cub.uca.edu>
2025-01-16 16:07:49 -0600

    init commit

--- src/Purse.java
+++ src/Purse.java
@@ -1,9 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
+
+
 public class Purse {
     int cash;
+
+
     public Purse(int cash){
         cash = 0;
     }
-    public void add(int cash){
 
+
+    public void add(int cash){
+        
     }
 }
